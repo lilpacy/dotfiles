53b38615e9a6f38a8d35516546dab08fff41f95a
lilpacy <revivedtomorrow@gmail.com>
2023-10-08 01:17:37 +0900

    シェルスクリプト追加

--- link.sh
+++ link.sh
@@ -3,3 +3,6 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
 ln -sf ~/dotfiles/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+
+chmod 700 ~/dotfiles/shell/task_cal
+sudo ln -sf ~/dotfiles/shell/task_cal /usr/local/bin/task_cal
