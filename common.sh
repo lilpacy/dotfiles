6738de23756e9dfa748749629c1efa9d0866b5cb
lilpacy <revivedtomorrow@gmail.com>
2025-12-03 03:01:49 +0900

    bedrockのclaudeを使うように設定

--- common.sh
+++ common.sh
@@ -182,3 +182,7 @@ export PATH="/Users/lilpacy/.codeium/windsurf/bin:$PATH"
 
 # llvm(for scan-build)
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+
+# claude code
+export CLAUDE_CODE_USE_BEDROCK=1
+export ANTHROPIC_MODEL='jp.anthropic.claude-sonnet-4-5-20250929-v1:0'
