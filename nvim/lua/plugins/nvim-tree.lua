4ef499dc6b7128beea571831f389557b0b9b4533
lilpacy <revivedtomorrow@gmail.com>
2025-12-06 16:35:34 +0900

    nvimのtreeビューを強化

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,38 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  config = true,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- アイコン表示用
+  },
+  config = function()
+    require("nvim-tree").setup({
+      -- ビューの設定
+      view = {
+        width = 30,
+        side = "left",
+      },
+      -- レンダラー設定
+      renderer = {
+        group_empty = true, -- 空ディレクトリをグループ化
+        icons = {
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+          },
+        },
+      },
+      -- フィルター設定
+      filters = {
+        dotfiles = false, -- .で始まるファイルを表示
+      },
+      -- git統合
+      git = {
+        enable = true,
+        ignore = false,
+      },
+    })
+  end,
   keys = {
     { "<C-n>", "<cmd>NvimTreeToggle<CR>", desc = "Toggle NvimTree" },
   },
