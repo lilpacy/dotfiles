c1e5cc39c59ca566d0b586a997c4c025eff19033
lilpacy <revivedtomorrow@gmail.com>
2025-12-06 15:21:35 +0900

    awsのdefaultプロファイルを変更

--- common.sh
+++ common.sh
@@ -87,7 +87,7 @@ export LC_MESSAGES='ja_JP.UTF-8'
 
 # aws
 export AWS_REGION=ap-northeast-1
-export AWS_PROFILE=default
+export AWS_PROFILE=sandbox-pacy
 
 # others
 #export LDFLAGS="-L/opt/homebrew/opt/bison/lib"
