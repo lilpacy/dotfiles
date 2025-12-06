30fbfdabd6c05ac4d6392c7b846456ee9bc71308
lilpacy <revivedtomorrow@gmail.com>
2025-12-06 16:58:18 +0900

    nvimもyankをクリップボードと同期

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
+-- 基本設定を読み込み
+require("config.options")
+
+-- プラグインマネージャーを読み込み
 require("config.lazy")
 
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- クリップボード連携を有効化
+vim.opt.clipboard = "unnamedplus"
+
+-- その他の基本設定
+vim.opt.number = true         -- 行番号を表示
+vim.opt.relativenumber = true -- 相対行番号を表示
+vim.opt.expandtab = true      -- タブをスペースに変換
+vim.opt.shiftwidth = 2        -- インデント幅
+vim.opt.tabstop = 2           -- タブ幅
