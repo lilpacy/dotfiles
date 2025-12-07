22558133445c287818078474e51f43466cb43d68
lilpacy <revivedtomorrow@gmail.com>
2025-12-08 00:20:09 +0900

    claude codeの設定を更新

--- common.sh
+++ common.sh
@@ -183,9 +183,3 @@ export PATH="/Users/lilpacy/.codeium/windsurf/bin:$PATH"
 # llvm(for scan-build)
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 
-# claude code
-export CLAUDE_CODE_USE_BEDROCK=1
-export ANTHROPIC_MODEL='jp.anthropic.claude-sonnet-4-5-20250929-v1:0'
-export CLAUDE_CODE_MAX_OUTPUT_TOKENS=4096
-export MAX_THINKING_TOKENS=1024
-
