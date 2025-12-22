22cfe1e69ee289fe8ed30747ca435ad72b59f616
lilpacy <revivedtomorrow@gmail.com>
2025-12-23 04:00:56 +0900

    コードジャンプできるミニマム設定

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,29 +10,105 @@ return {
       -- Masonのセットアップ
       require("mason").setup()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" }, -- 使用する言語に応じて追加
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",           -- TypeScript/JavaScript
+          "tailwindcss",     -- Tailwind CSS (Next.jsでよく使う)
+          "eslint",          -- ESLint
+          "jsonls",          -- JSON (package.json, tsconfig.jsonなど)
+        },
         automatic_installation = true,
       })
 
       -- LSPキーマッピング
       local on_attach = function(client, bufnr)
-        local opts = { buffer = bufnr, noremap = true, silent = true }
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts) -- 定義へジャンプ
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts) -- 宣言へジャンプ
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts) -- 実装へジャンプ
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts) -- 参照を表示
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts) -- ホバー情報
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- リネーム
+        local bufmap = function(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, desc = desc })
+        end
+
+        -- 基本のキーマップ
+        bufmap('n', 'gd', vim.lsp.buf.definition, '定義へジャンプ')
+        bufmap('n', 'gi', vim.lsp.buf.implementation, '実装へジャンプ')
+        bufmap('n', 'gr', vim.lsp.buf.references, '参照を表示')
+        bufmap('n', 'K', vim.lsp.buf.hover, 'ホバー情報')
+        bufmap('n', '<leader>rn', vim.lsp.buf.rename, 'リネーム')
+        bufmap('n', 'gt', vim.lsp.buf.type_definition, '型定義へジャンプ')
+
+        -- declarationはサーバーが対応している場合のみ
+        if client.supports_method('textDocument/declaration') then
+          bufmap('n', 'gD', vim.lsp.buf.declaration, '宣言へジャンプ')
+        end
+
+        -- Cmd+Click で定義にジャンプ (VSCode風)
+        bufmap('n', '<D-LeftMouse>', function()
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<LeftMouse>', true, false, true), 'n', false)
+          vim.lsp.buf.definition()
+        end, 'Cmd+Click で定義へ')
+
+        -- Ctrl+Click でも定義にジャンプ
+        bufmap('n', '<C-LeftMouse>', function()
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<LeftMouse>', true, false, true), 'n', false)
+          vim.lsp.buf.definition()
+        end, 'Ctrl+Click で定義へ')
       end
 
       -- LSPサーバーの設定
-      vim.lsp.config('lua_ls', {
-        on_attach = on_attach
+      local lspconfig = require('lspconfig')
+
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' }
+            }
+          }
+        }
       })
-      vim.lsp.config('ts_ls', {
-        on_attach = on_attach
+
+      -- TypeScript/JavaScript (Next.js対応)
+      lspconfig.ts_ls.setup({
+        on_attach = on_attach,
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = 'all',
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            }
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = 'all',
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            }
+          }
+        }
       })
-      -- 他の言語サーバーも同様に追加
+
+      -- Tailwind CSS
+      lspconfig.tailwindcss.setup({
+        on_attach = on_attach,
+      })
+
+      -- ESLint
+      lspconfig.eslint.setup({
+        on_attach = on_attach,
+      })
+
+      -- JSON
+      lspconfig.jsonls.setup({
+        on_attach = on_attach,
+      })
     end,
   },
 }
