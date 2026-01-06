8f17198c7c03bb2aa383a1613d0bb58533be74b5
lilpacy <revivedtomorrow@gmail.com>
2026-01-07 06:15:20 +0900

    nvimでmermaidをプレビューできるように

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,40 @@
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  priority = 1000,
+  ---@type snacks.Config
+  opts = {
+    image = {
+      enabled = true,
+      doc = {
+        enabled = true,
+        inline = true,
+        float = true,
+        max_width = 80,
+        max_height = 40,
+      },
+      convert = {
+        notify = true,
+        mermaid = function()
+          local theme = vim.o.background == "light" and "neutral" or "dark"
+          return {
+            "-i", "{src}",
+            "-o", "{file}",
+            "-b", "transparent",
+            "-t", theme,
+            "-s", "{scale}",
+          }
+        end,
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>mi",
+      function()
+        Snacks.image.hover()
+      end,
+      desc = "Show image at cursor",
+    },
+  },
+}
