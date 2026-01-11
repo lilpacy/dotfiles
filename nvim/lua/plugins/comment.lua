9ac312cbc70590408882e0904ca1b911265c985e
lilpacy <revivedtomorrow@gmail.com>
2026-01-11 20:50:37 +0900

    nvimでもvscodeみたいにctrl+/でアンコメント、コメントアウト

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -2,9 +2,23 @@ return {
   "numToStr/Comment.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("Comment").setup({
+    local comment = require("Comment")
+    comment.setup({
       -- gc でコメント切り替え（行コメント）
       -- gb でブロックコメント
+    })
+
+    local api = require("Comment.api")
+    vim.keymap.set("n", "<C-_>", api.toggle.linewise.current, {
+      desc = "Toggle comment (VSCode style)",
+    })
+
+    local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+    vim.keymap.set("x", "<C-_>", function()
+      vim.api.nvim_feedkeys(esc, "nx", false)
+      api.toggle.linewise(vim.fn.visualmode())
+    end, {
+      desc = "Toggle comment (VSCode style)",
     })
   end,
 }
