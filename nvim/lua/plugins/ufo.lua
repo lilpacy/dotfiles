4e2e6e3d927b859e93770737d4e3d1d99e9007ba
lilpacy <revivedtomorrow@gmail.com>
2026-03-19 20:07:52 +0900

    nvim: ufo keymapsをconfig内に移動してS-Tab問題を修正

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -15,34 +15,36 @@ return {
   "kevinhwang91/nvim-ufo",
   dependencies = { "kevinhwang91/promise-async" },
   event = "BufReadPost",
-  keys = {
+  config = function()
+    require("ufo").setup({
+      provider_selector = function()
+        return { "treesitter", "indent" }
+      end,
+    })
+
+    local ufo = require("ufo")
+    local map = vim.keymap.set
+
     -- Intuitive shortcuts
-    { "<Tab>", "za", desc = "Toggle fold" },
-    { "<S-Tab>", function()
-      local ufo = require("ufo")
-      -- foldlevel=0 means all closed → open, otherwise close
+    map("n", "<Tab>", "za", { desc = "Toggle fold" })
+    map("n", "<S-Tab>", function()
       if vim.wo.foldlevel == 0 then
         ufo.openAllFolds()
       else
         ufo.closeAllFolds()
       end
-    end, desc = "Toggle all folds" },
-    { "K", function()
-      local winid = require("ufo").peekFoldedLinesUnderCursor()
+    end, { desc = "Toggle all folds" })
+    map("n", "K", function()
+      local winid = ufo.peekFoldedLinesUnderCursor()
       if not winid then
         vim.lsp.buf.hover()
       end
-    end, desc = "Peek fold or hover" },
+    end, { desc = "Peek fold or hover" })
 
     -- Standard z-commands (remapped to ufo for consistency)
-    { "zR", function() require("ufo").openAllFolds() end, desc = "Open all folds" },
-    { "zM", function() require("ufo").closeAllFolds() end, desc = "Close all folds" },
-    { "zr", function() require("ufo").openFoldsExceptKinds() end, desc = "Reduce folding" },
-    { "zm", function() require("ufo").closeFoldsWith() end, desc = "Fold more" },
-  },
-  opts = {
-    provider_selector = function()
-      return { "treesitter", "indent" }
-    end,
-  },
+    map("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
+    map("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
+    map("n", "zr", ufo.openFoldsExceptKinds, { desc = "Reduce folding" })
+    map("n", "zm", ufo.closeFoldsWith, { desc = "Fold more" })
+  end,
 }
