cb0c846d1b69786f9ea9056a060ad6fffdf8f8ce
lilpacy <revivedtomorrow@gmail.com>
2026-03-21 02:29:09 +0900

    nvim: diffview.nvimにキーマップ追加（leader+g系）

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,5 +1,11 @@
 return {
   "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+    { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History" },
+    { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview Branch History" },
+    { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+  },
   config = function()
     require("diffview").setup({
       view = {
